9049b7be5bf933bb27b29110f085b101412a383e
j-tan871 <60245545+j-tan871@users.noreply.github.com>
2021-06-12 00:44:07 -0500

    styling

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import "./assets/vendor/nucleo/css/nucleo.css";
+import "./assets/vendor/font-awesome/css/font-awesome.min.css";
+import "./assets/css/argon-design-system-react.css";
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
