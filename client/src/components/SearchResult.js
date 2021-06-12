5960a08fdca42e2ba6ad9eb54bd8813d76229b35
catbat127 <catherine.yang127@gmail.com>
2021-06-12 16:45:37 -0500

    done

--- client/src/components/SearchResult.js
+++ client/src/components/SearchResult.js
@@ -2,6 +2,25 @@ import {useState} from 'react';
 import { Card, Button } from 'reactstrap';
 
 export default function SearchResult(props){
+    async function handleSave(){
+        const headers = {
+            'Access-Control-Allow-Origin' : '*', 
+            'Content-Type': 'application/json'
+        };
+        try{
+            await fetch(`http://localhost:5000/save`, {
+                headers: headers,
+                method: 'POST',
+                body: JSON.stringify({
+                    name: props.name,
+                    address: props.address,
+                    rating: props.rating
+                })
+            });
+        } catch(err){
+            console.log(err);
+        }
+    }
     return(
         <div>
             <Card className='p-3 m-3 card-lift--hover shadow border-0'>
@@ -14,7 +33,9 @@ export default function SearchResult(props){
                 <p>
                     Rating: {props.rating}/5 stars
                 </p>
-                <Button color='primary'>Save</Button>
+                {
+                    props.hasButton ? <Button color='primary' onClick={handleSave}>Save</Button> : null
+                }
             </Card>
         </div>
     )
--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,4 +1,5 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import SearchResult from '../components/SearchResult';
 
 export default function Saved() {
     useEffect(() => {
@@ -14,13 +15,26 @@ export default function Saved() {
             const responseData = await response.json();
             console.log(responseData['saved']);
             console.log('done')
+            setSavedItems(responseData['saved']);
         }
         fetchSavedLocations();
     }, []);
 
+    const [savedItems, setSavedItems] = useState()
     return (
         <div className='m-5'>
             <h1 className='display-1'>Saved Locations</h1>
+            { savedItems ? savedItems.map((item, id) => 
+                <SearchResult 
+                    key={id}
+                    name={item.name}
+                    address={item.address}
+                    rating={item.rating}
+                    hasButton={false}
+                    className="order-lg-2 mt-4" lg="5"
+                    />
+            ) : null 
+            }
         </div>
     )
 }
