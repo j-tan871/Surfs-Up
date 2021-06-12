932d5a286f43bacda4944e9365a387c0ee27d929
j-tan871 <60245545+j-tan871@users.noreply.github.com>
2021-06-12 15:41:11 -0500

    saved

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,4 +1,16 @@
-export default function Saved(){
+import { useEffect } from 'react';
+
+export default function Saved() {
+    useEffect(() => {
+        async function fetchSavedLocations() {
+            const response = await fetch('http://localhost:5000/save');
+            // converts to a Javascript object
+            const responseData = await response.json();
+            console.log(responseData);
+        }
+        fetchSavedLocations();
+    }, []);
+
     return (
         <div className='m-5'>
             <h1 className='display-1'>Saved Locations</h1>
