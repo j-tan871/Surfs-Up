d02acbd660a06465d8e493f4e5e9e6fdb01a10cd
j-tan871 <60245545+j-tan871@users.noreply.github.com>
2021-06-12 15:01:49 -0500

    json error

--- server/app.py
+++ server/app.py
@@ -1,7 +1,14 @@
 from flask import Flask, request
 import pymongo
+import json
+from bson import ObjectId
 
 app = Flask(__name__)
+class JSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
 
 @app.route("/")
 def hello_world():
@@ -23,8 +30,14 @@ def save():
 
 @app.route("/find")
 def find():
+    client = pymongo.MongoClient("mongodb+srv://catbat127:mongoDB@cluster0.1tnsl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&ssl_cert_reqs=CERT_NONE")
+    #db is the test database in client, creates it if it does not exist yet
+    db = client.test
+
+    #goes the test database and goes into/creates testCollection table
+    collection = db.testCollection
     x = collection.find()
     result = []
     for record in x:
         result.append(record)
-    return {"saved": result}
+    return json.encode({"saved": result}, cls=JSONEncoder)
