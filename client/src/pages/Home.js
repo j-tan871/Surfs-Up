3a0f58d1a8555200b2956dae145f9bf4d0c47f1e
catbat127 <catherine.yang127@gmail.com>
2021-06-12 08:44:47 -0500

    results

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import SearchResult from '../components/SearchResult';
 function Home() {
     const [category, setCategory] = useState('');
     const [location, setLocation] = useState('');
@@ -14,28 +15,41 @@ function Home() {
     }
 
     function handleSubmit(event) {
-        console.log('hi')
+        console.log('hi');
         alert('submitted');
         event.preventDefault();
         setCategory('');
         setLocation('');
     }
 
+    const info = [
+        {
+            name: 'Paris Baguette',
+            address: '2300 Main St',
+            number: '1234567890'
+        }
+    ]
     return (
         <div>
             <h1>
                 Find locations of interest:
             </h1>
             <h4>
-                Find <input type="text" placeholder='Ex: Restaurants' value={category} 
-                onChange={handleCategoryChange}></input> at <input 
-                    type="text" placeholder='Ex: Miami Beach' value={location} 
-                    onChange={handleLocationChange}></input>
-                    <button onClick={handleSubmit}>Submit</button>
+                Find <input type="text" placeholder='Ex: Restaurants' value={category}
+                    onChange={handleCategoryChange}></input> at <input
+                        type="text" placeholder='Ex: Miami Beach' value={location}
+                        onChange={handleLocationChange}></input>
+                <button onClick={handleSubmit}>Submit</button>
             </h4>
-            
+            {info.map((item, id) => <SearchResult
+                key = {id}
+                name = {item.name}
+                address = {item.address}
+                numBer = {item.number}
+                />)
+            }
         </div>
-        
+
     )
 }
 export default Home;
