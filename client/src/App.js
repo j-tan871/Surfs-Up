543296c0ab42b26572739c373416800468125a8f
catbat127 <catherine.yang127@gmail.com>
2021-06-12 12:25:54 -0500

    react router

--- client/src/App.js
+++ client/src/App.js
@@ -3,28 +3,51 @@ import './App.css';
 import Result from './components/Result'
 import Catherine from './components/Catherine'
 import Home from './pages/Home'
+import Saved from './pages/Saved'
 import EssayForm from './test'
 import { NavItem } from 'reactstrap';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   // const searchresults = ['apple', 'pear', 'banana'];
   // const faces = [':D', ':%^)', '>:C']
 
-  const info = [
-    {
-      location: 'Paris Baguette',
-      address: '2300 Main St', 
-      number:'214-234-2341'
-    }, 
-  ]
-
   return (
     <div>
-      <h1 className='display-1 m-5'>Trip Planner</h1>
-      <Home />
-    </div>
-    // <div>
-    //   {
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/saved">Saved Items</Link>
+              </li>
+            </ul>
+          </nav>
+          <h1 className='display-1 m-5'>Trip Planner</h1>
+          
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/saved">
+              <Saved />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+
+            </div>
     //     searchresults.map((result) => 
     //     <Result 
     //       fruit={result}
