b31eaa4b31252257e935def4c23cd2e446f06d5c
catbat127 <catherine.yang127@gmail.com>
2021-06-12 07:57:36 -0500

    search bar

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,41 @@
+import { useState } from 'react';
 function Home() {
+    const [category, setCategory] = useState('');
+    const [location, setLocation] = useState('');
+
+    function handleCategoryChange(event) {
+        setCategory(event.target.value);
+        console.log(category);
+    }
+
+    function handleLocationChange(event) {
+        setLocation(event.target.value);
+        console.log(location);
+    }
+
+    function handleSubmit(event) {
+        console.log('hi')
+        alert('submitted');
+        event.preventDefault();
+        setCategory('');
+        setLocation('');
+    }
+
     return (
-        <h1>
-            Find locations of interest:
-        </h1>
+        <div>
+            <h1>
+                Find locations of interest:
+            </h1>
+            <h4>
+                Find <input type="text" placeholder='Ex: Restaurants' value={category} 
+                onChange={handleCategoryChange}></input> at <input 
+                    type="text" placeholder='Ex: Miami Beach' value={location} 
+                    onChange={handleLocationChange}></input>
+                    <button onClick={handleSubmit}>Submit</button>
+            </h4>
+            
+        </div>
+        
     )
 }
 export default Home;
